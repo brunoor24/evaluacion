4b4a58a18fb0c8cb67d2a82713895aa80f5f4544
Marco01373782 <116099860+Marco01373782@users.noreply.github.com>
2022-11-16 09:55:44 -0500

    se agrego put y patch

--- src/main/kotlin/com/example/leccion/controller/AttendeeController.kt
+++ src/main/kotlin/com/example/leccion/controller/AttendeeController.kt
@@ -3,29 +3,35 @@ package com.example.leccion.controller
 import com.example.leccion.model.Attendee
 import com.example.leccion.service.AttendeeService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 
-    @RestController
-    @RequestMapping("/attendee")
-    class AttendeeController {
+@RestController
+@RequestMapping("/attendee")
+class AttendeeController {
+    @Autowired
+    lateinit var attendeeService: AttendeeService
 
-        @Autowired
-        lateinit var attendeeService: AttendeeService
+    @GetMapping
+    fun list():List<Attendee>{
+        return attendeeService.list()
+    }
 
-        @GetMapping
-        fun list():List<Attendee>{
-            return attendeeService.list()
-        }
+    @PostMapping
+    fun save(@RequestBody attendee: Attendee):Attendee? {
+        return attendeeService.save(attendee)
+    }
 
-        @PostMapping
-        fun save(@RequestBody attendee: Attendee):Attendee? {
-            return attendeeService.save(attendee)
-        }
+    @PutMapping
+    fun update (@RequestBody attendee:Attendee): ResponseEntity<Attendee>{
+        return  ResponseEntity(attendeeService.update(attendee), HttpStatus.OK)
+    }
 
-        }
+    @PatchMapping
+    fun updateName (@RequestBody attendee:Attendee):ResponseEntity<Attendee>{
+        return ResponseEntity(attendeeService.updateName(attendee), HttpStatus.OK)
+    }
+}
 
--- src/main/kotlin/com/example/leccion/service/AttendeeService.kt
+++ src/main/kotlin/com/example/leccion/service/AttendeeService.kt
@@ -3,21 +3,45 @@ package com.example.leccion.service
 import com.example.leccion.model.Attendee
 import com.example.leccion.repository.AttendeeRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class AttendeeService {
+    @Autowired
+    lateinit var attendeeRepository: AttendeeRepository
 
+    fun list ():List<Attendee>{
+        return attendeeRepository.findAll()
+    }
 
-        @Autowired
-        lateinit var attendeeRepository: AttendeeRepository
+    fun save (attendee: Attendee):Attendee{
+        return attendeeRepository.save(attendee)
+        attendeeRepository.findById(attendee.id)?: throw Exception("Id no existe")
+    }
 
-        fun list ():List<Attendee>{
-            return attendeeRepository.findAll()
-        }
-
-        fun save (attendee: Attendee):Attendee{
+    fun update(attendee: Attendee):Attendee{
+        try {
+            attendeeRepository.findById(attendee.id)
+                    ?: throw Exception("Id no existe")
             return attendeeRepository.save(attendee)
         }
-
+        catch(ex:Exception){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, ex.message)
+        }
+    }
+    fun updateName(attendee:Attendee): Attendee {
+        try{
+            val response = attendeeRepository.findById(attendee.id)
+                    ?: throw Exception("ID no existe")
+            response.apply {
+                name=attendee.name
+            }
+            return attendeeRepository.save(response)
+        }
+        catch (ex:Exception){
+            throw ResponseStatusException(HttpStatus.NOT_FOUND,ex.message)
+        }
+    }
 }
